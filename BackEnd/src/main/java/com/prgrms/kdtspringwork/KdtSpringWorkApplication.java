07380d33b0f60d057d50c58736daf867034afed1
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-25 16:16:59 +0900

    init : 백엔드 프로젝트 세팅

---
+++ BackEnd/src/main/java/com/prgrms/kdtspringwork/KdtSpringWorkApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.kdtspringwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtSpringWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtSpringWorkApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/prgrms/kdtspringwork/KdtSpringWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.kdtspringwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtSpringWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
