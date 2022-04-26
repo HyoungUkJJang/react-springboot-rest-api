fe69f819bc97d369bb90e14c7edccb85925879f7
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-26 15:42:49 +0900

    feat : cors 설정 작성 / 상품 restApi 컨트롤러 작성

---
+++ BackEnd/src/main/java/com/prgrms/kdtspringwork/controller/api/ProductRestController.java
@@ -0,0 +1,28 @@
+package com.prgrms.kdtspringwork.controller.api;
+
+import com.prgrms.kdtspringwork.model.Category;
+import com.prgrms.kdtspringwork.model.Product;
+import com.prgrms.kdtspringwork.service.ProductService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestController {
+
+    private final ProductService productService;
+
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/api/v1/products")
+    public List<Product> productList(@RequestParam Optional<Category> category) {
+        return category
+            .map(productService::getProductsByCategory)
+            .orElse(productService.getAllProducts());
+    }
+
+}
